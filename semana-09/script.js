6578a4d2c43f7ce077a4c6c2feee598e68ff86a6
elem3d <elem3d@users.noreply.github.com>
2026-08-26 21:29:47 -0300

    feat(form): script

--- semana-09/script.js
+++ semana-09/script.js
@@ -1,5 +1,31 @@
 const form = document.getElementById("formularioDeInscricao");
 
+const resumo = document.createElement("section");
+resumo.id ="resumo";
+
+formulario.arter(resumo);
+
 form.addEventListener("submit", function(event){
     event.preventDefault();
+
+    
+    const interessesInput = []
+    document.querySelectorAll('input[name = "temas"]:checked').forEach(tema => {interessesInput.push(tema.value);});
+
+    
+
+    const dadosDoInscrito = {
+        nome: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value,
+        idade: document.getElementById("idade").value,
+        dataNascimento: document.getElementById("nascimento").value,
+        apresentacao: document.getElementById("apresentacao") .value,
+        trilha: document.getElementById("trilha").value,
+        nivelConhecimento: document.querySelector('input[name = "nível"]:checked').value,
+        areasDeInteresse: interessesInput
+    }
+
+    resumo.innerHTML = "<h2>Resumo de Inscrição:<h2/>"
 });
+
