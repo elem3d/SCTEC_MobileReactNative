f1a4ed79a6c69f4449272a887f6f4229abb1cddc
elem3d <elem3d@users.noreply.github.com>
2026-08-28 22:07:54 -0300

    feat(form): validação de campos

--- semana-09/script.js
+++ semana-09/script.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 const form = document.getElementById("formularioDeInscricao");
 
 const resumo = document.createElement("section");
@@ -8,23 +10,82 @@ form.after(resumo);
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
-
+    const nomeInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const senhaInput = document.getElementById("senha");
+    const idadeInput = document.getElementById("idade");
+    const nascimentoInput = document.getElementById("nascimento");
+    const apresentacaoInput = document.getElementById("apresentacao");
+    const trilhaInput = document.getElementById("trilha");
+    const conhecimentoInput = document.querySelector('input[name = "nível"]:checked');
+    
     const interessesInput = []
     document.querySelectorAll('input[name = "temas"]:checked').forEach(tema => {interessesInput.push(tema.value);});
 
-    
 
     const dadosDoInscrito = {
-        nome: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        senha: document.getElementById("senha").value,
-        idade: document.getElementById("idade").value,
-        dataNascimento: document.getElementById("nascimento").value,
-        apresentacao: document.getElementById("apresentacao") .value,
-        trilha: document.getElementById("trilha").value,
-        nivelConhecimento: document.querySelector('input[name = "nível"]:checked').value,
+        nome: nomeInput.value,
+        email: emailInput.value,
+        senha: senhaInput.value,
+        idade: idadeInput.value,
+        dataNascimento: nascimentoInput.value,
+        apresentacao:apresentacaoInput.value,
+        trilha: trilhaInput.value,
+        nivelConhecimento:conhecimentoInput.value,
         areasDeInteresse: interessesInput
     }
+    
+    const validacao = (dadosInscrito) =>{
+        //Definindo as regras:
+        const senhaRegra = /^(?=.*[0-9])(?=.*[A-Z]).{8,}$/;
+        const emailRegra = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        const apresentacaoRegra = /(?:\S\s*){21,}/
+
+        let erros = [];
+
+        //Verificações:
+        if(!senhaRegra.test(dadosInscrito.senha)){
+            erros.push({
+                error: 1,
+                campo: senhaInput,
+                mensagem: "A senha deve ter ao menos um número, uma letra maiúscula e 8 caracteres"
+            });
+        }
+        if(!emailRegra.test(dadosInscrito.email)){
+            erros.push({
+                erro: 2,
+                campo: emailInput,
+                mensagem: "Insira um email válido"
+            });
+        }
+        if(!apresentacaoRegra.test(dadosInscrito.apresentacao)){
+            erros.push({
+                erro: 3,
+                campo: apresentacaoInput,
+                mensagem: "A apresentação deve conter ao menos 20 caracteres"
+            });
+        }if(dadosInscrito.areasDeInteresse.length < 1){
+            erros.push({
+                erro: 4,
+                campo: interessesInput,
+                mensagem: "Selecione ao menos 1 campo de interesse"
+            })
+        }
+
+
+        erros.forEach((erro) => {
+            console.log(`Error: ${erro.erro}`);
+            erro.campo.classList.add("inputErro");
+
+            const mensagem = createElement("span");
+            mensagem.classList.add("mensagem-erro");
+            mensagem.textContent = erro.mensagem;
+            const campo = erro.campo.closest("fieldset") || closest(erro.campo)
+            campo.appendChild(mensagem)
+        })
+    };
+
+    validacao(dadosDoInscrito);
 
     resumo.innerHTML = `<h2>Resumo da Inscrição: ${dadosDoInscrito.nome}</h2>
         <p><strong>Nome Completo:</strong> ${dadosDoInscrito.nome}</p>
